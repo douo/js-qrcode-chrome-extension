4284593425da6228cbdb05a3f48356f2060b55dd
Tiou Lims <dourokinga@gmail.com>
2014-06-20 07:03:56 +0800

    utf-8 encode

--- popup.js
+++ popup.js
@@ -21,27 +21,53 @@
     }
 
     function generete(text){
-	option.text = text;
+	// utf8 支持
+	option.text = String(text).replace(
+		/[\u0080-\u07ff]/g,
+		function(c) {
+			var cc = c.charCodeAt(0);
+			return String.fromCharCode(0xc0 | cc >> 6, 0x80 | cc & 0x3f);
+		}
+	).replace(
+		/[\u0800-\uffff]/g,
+		function(c) {
+			var cc = c.charCodeAt(0);
+			return String.fromCharCode(0xe0 | cc >> 12, 0x80 | cc >> 6 & 0x3f, 0x80 | cc & 0x3f);
+		}
+	);
 	clearCanvas();
 	$("canvas").qrcode(option);
     }
-
+    var status = "init";
     function showInput(){
-	$(".result").hide();
-	$(".btns").show();
-	$(".text").height(225);
-	$(".input").width(256);
-	$(".input").height($(".text").height()+$(".btns").height());
+	if(status != "input"){
+	    $(".result").hide();
+	    $(".btns").show();
+	    $(".text").height(225);
+	    $(".input").width(256);
+	    $(".input").height($(".text").height()+$(".btns").height());
+	    
+	    status = "input";
+	}
     }
     function showCode(){
-	$(".btns").hide();
-	$(".text").height(18);
-	$(".input").height($(".text").height());
-	$(".result").show();
+	if(status != "code"){
+	    $(".btns").hide();
+	    $(".text").height(18);
+	    $(".input").height($(".text").height());
+	    $(".result").show();
+	    
+	    status = "code";
+	}
     }
 
     function init(){
-	$(".text").focus(showInput);
+	$(".text").click(showInput);
+	/* 
+	   有可能在某种情况下，页面弹出时，text 会立刻获得焦点
+	   比如 Mac ，但给 canvas 加了 tabindex 后就可以避免
+	*/
+	setTimeout(function(){$(".text").focus(showInput)},50);
 	$("#cancel").click(showCode);
 	$("#ok").click(function(){
 	    generete($(".text").val());
