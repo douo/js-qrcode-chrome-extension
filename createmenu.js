a3a5505040b6f4d5ee39eceefc30d44b85789bda
Tiou Lims <dourokinga@gmail.com>
2014-06-20 07:04:14 +0800

    add context menu support

---
+++ createmenu.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+      console.log("item " + info.menuItemId + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+    var data = "";
+    if(info.menuItemId == "jsqr-page"){
+	data = info.pageUrl;
+    }else if(info.menuItemId == "jsqr-selection"){
+	data = info.selectionText;
+    }else if(info.menuItemId == "jsqr-link"){
+	data = info.linkUrl;
+    }
+    console.log("data:"+data);
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  // Create one test item for each context type.
+  var contexts = ["page","selection","link"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+    var title = "Test '" + context + "' menu item";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "jsqr-" + context});
+    console.log("'" + context + "' item:" + id);
+  }
+});
